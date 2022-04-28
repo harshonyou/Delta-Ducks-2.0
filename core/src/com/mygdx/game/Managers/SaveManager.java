f12c5513f3669596497b57f262a2dcd9a182a8a9
Harsh Mohan <harshmohan813@gmail.com>
2022-04-28 18:47:00 +0100

    Add save manager to save player stats

--- core/src/com/mygdx/game/Managers/SaveManager.java
+++ core/src/com/mygdx/game/Managers/SaveManager.java
@@ -4,6 +4,7 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Preferences;
 import com.badlogic.gdx.math.Vector2;
 import com.mygdx.game.Components.Pirate;
+import com.mygdx.game.Entitys.Building;
 import com.mygdx.game.Entitys.College;
 import com.mygdx.game.Entitys.Ship;
 
@@ -33,6 +34,13 @@ public class SaveManager {
 //        prefs.putString("Ships", ships.toString());
 
         prefs.putString("Difficulty", DifficultyManager.getCurrentDifficulty());
+
+        // Player stats
+        prefs.putInteger("PlayerHealth", GameManager.getPlayer().getHealth());
+        prefs.putInteger("PlayerArmor", GameManager.getPlayer().getArmor());
+        prefs.putInteger("PlayerPlunder", GameManager.getPlayer().getPlunder());
+        prefs.putInteger("PlayerAmmo", GameManager.getPlayer().getAmmo());
+        prefs.putInteger("PlayerXp", GameManager.getPlayer().getXp());
 
         prefs.flush();
     }
@@ -68,10 +76,17 @@ public class SaveManager {
             default:
                 DifficultyManager.Initialise(DifficultyManager.Difficulty.HARD);
         }
+
+        // Player stats prefs.getInteger("")
+        GameManager.getPlayer().setHealth(prefs.getInteger("PlayerHealth"));
+        GameManager.getPlayer().setArmor(prefs.getInteger("PlayerArmor"));
+        GameManager.getPlayer().setPlunder(prefs.getInteger("PlayerPlunder"));
+        GameManager.getPlayer().setAmmo(prefs.getInteger("PlayerAmmo"));
+        GameManager.getPlayer().setXp(prefs.getInteger("PlayerXp"));
+
     }
 
     public static void test() {
         Preferences prefs = Gdx.app.getPreferences("saved_preference");
-
     }
 }
